15ce03ac664dcda84dea849d8f9ac094efd96a9d
AbhijeetHanwate <123650663+AbhijeetHanwate@users.noreply.github.com>
2023-01-30 18:18:58 +0530

    FirstProject

---
+++ Demoproject/src/mypackage/MyClass.java
@@ -0,0 +1,12 @@
+package mypackage;
+
+public class MyClass {
+public static void main(String[]args)
+		{
+			System.out.println("Team lead created maven project");
+			System.out.println("Page Layer class");
+			System.out.println("Utilities class");
+			System.out.println("Test Base Class");
+			System.out.println("Test Layer class");
+		}
+}
